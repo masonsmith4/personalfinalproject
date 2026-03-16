fb580ec278eef695b223f067592f9ed9802dd82e
ksmit <korie.smith@bsu.edu>
2026-03-15 23:38:01 -0400

    Created the method to make the 3rd test pass.

--- lib/url_builder.dart
+++ lib/url_builder.dart
@@ -4,10 +4,14 @@ class UrlBuilder {
         '/riot/account/v1/accounts/by-riot-id/${Uri.encodeComponent(username)}/${Uri.encodeComponent(tag)}'
     );
   }
-
   Uri buildMatchId(String puuid) {
     return Uri.parse(
         'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${Uri.encodeComponent(puuid)}/ids'
+    );
+  }
+  Uri buildMatchUrl(String matchId) {
+    return Uri.parse(
+        'https://americas.api.riotgames.com/lol/match/v5/matches/${Uri.encodeComponent(matchId)}'
     );
   }
 }
