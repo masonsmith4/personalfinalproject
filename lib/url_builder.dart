148fd6fa5d1f0a49a493b46ee434907a4a006b3c
ksmit <korie.smith@bsu.edu>
2026-03-15 22:49:45 -0400

    Created a method within the url builder class to make the test pass.

--- lib/url_builder.dart
+++ lib/url_builder.dart
@@ -1,7 +1,13 @@
 class UrlBuilder {
   Uri buildUrl(String username, String tag) {
     return Uri.parse('https://americas.api.riotgames.com'
-        '/riot/account/v1/accounts/by-riot-id/${Uri.encodeComponent(username)}'
-        '/${Uri.encodeComponent(tag)}');
+        '/riot/account/v1/accounts/by-riot-id/${Uri.encodeComponent(username)}/${Uri.encodeComponent(tag)}'
+    );
+  }
+
+  Uri buildMatchId(String puuid) {
+    return Uri.parse(
+        'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${Uri.encodeComponent(puuid)}/ids'
+    );
   }
 }
