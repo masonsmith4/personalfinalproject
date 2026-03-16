faceb07aedc6775e707efa933a4ad3993bc155da
ksmit <korie.smith@bsu.edu>
2026-03-15 21:42:57 -0400

    Added JSON file for sample data and refactored the initial test slightly.

--- test/url_builder_test.dart
+++ test/url_builder_test.dart
@@ -4,9 +4,12 @@ void main() {
   test('Builds URL for the PUUID', () {
     final constructor = UrlBuilder();
     final uri = constructor.buildUrl();
+    final uri = constructor.buildUrl('WombatBaby', 'NA2');
     expect(uri.scheme, 'https');
     expect(uri.host, 'americas.api.riotgames.com');
     expect(uri.path, contains('username'));
     expect(uri.path, contains('tag'));
+    expect(uri.path, contains('WombatBaby'));
+    expect(uri.path, contains('NA2'));
   });
 }
