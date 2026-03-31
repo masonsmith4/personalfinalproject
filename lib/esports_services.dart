f70346bb2566e2fc19c2b746c8228ec787ff48b3
Mason Smith <msmithbsu4@gmail.com>
2026-03-31 14:15:53 -0400

    tests for network error

--- lib/esports_services.dart
+++ lib/esports_services.dart
@@ -30,18 +30,18 @@ class EsportsService {
   }
   //Returns the puuid of the user as a String
   Future<String> requestIdentification(String username, String tag) async {
-    final response = await http.get(_urlBuilder.buildUrl(username, tag), headers: _headers);
+    final response = await _sendRequest(_urlBuilder.buildUrl(username, tag));
     return _parser.parseIdentification(response.body);
   }
   //Returns a list of match ids as Strings
   Future<List<String>> requestMatchList(String puuid) async {
-    final response = await http.get(_urlBuilder.buildMatchId(puuid), headers: _headers);
+    final response = await _sendRequest(_urlBuilder.buildMatchId(puuid));
     return _parser.parseMatchList(response.body);
   }
   //Returns a dictionary of data within the match
   //Key is a String, value can be either a String, int, or a float
   Future<Map<String, dynamic>> requestMatchData(String matchId) async {
-    final response = await http.get(_urlBuilder.buildMatchUrl(matchId), headers: _headers);
+    final response = await _sendRequest(_urlBuilder.buildMatchUrl(matchId));
     return _parser.parseMatchData(response.body);
   }
 }
--- test/esports_service_test.dart
+++ test/esports_service_test.dart
@@ -1,10 +1,15 @@
 import 'package:test/test.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:finalproject_npinkelton_ksmith_nsmith_msmith/esports_services.dart';
 
 void main() {
-  test ('Check that network is running', () async {
-    final network = EsportsService();
-    final puuid = await network.requestIdentification('WombatBaby', 'NA2');
+  setUpAll(() async {
+    await dotenv.load(fileName: '.env');
+  });
+
+  test('Check that network is running', () async {
+    final service = EsportsService();
+    final puuid = await service.requestIdentification('WombatBaby', 'NA2');
 
     expect(puuid, isNotEmpty);
   });
